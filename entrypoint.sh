ac9d3c7326fbcadcb806753ffbeaec1203452be1
thandal <than@timbrel.org>
2021-08-09 17:39:00 -0400

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Configuration: MEM=$MEM DISK=$DISK"
 
 # Create the ext4 volume image for UML to mount as /var/lib/containers
 # TODO: clean up this workaround for some issue with overlayfs and buildah.
-if [ ! -f /persistent/var_lib_containers.img ]; then
+if [ ! -f /var_lib_containers.img ]; then
     echo "Formatting /var_lib_docker.img"
     dd if=/dev/zero of=/var_lib_containers.img bs=1 count=0 seek=${DISK} > /dev/null 2>&1
     mkfs.ext4 /var_lib_containers.img > /dev/null 2>&1
